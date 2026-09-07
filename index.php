c47b3b1a874aa55fff3e870d01d9331dd8cc8057
ahmmedabdulkadir25-gif <ahmmedabdulkadir25@gmail.com>
2026-09-07 14:40:43 +0200

    .php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 /*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
 /*    Programmet skriver ut en "god dag"-melding med personens navn 
 */
-  $fornavn=$_POST ["fornavn"];
-  $etternavn=$_POST ["etternavn"];  
+  $fornavn=$_POST["fornavn"];
+  $etternavn=$_POST["etternavn"];  
 	
   print ("God dag $fornavn $etternavn <br />");  
 ?>
